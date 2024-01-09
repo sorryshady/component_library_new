7584537267316880884eece7a10ba769d7282a82
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-01-09 22:50:45 +0530

    hover_landing vanilla js part 2 completed

--- javascript/landing_pages/hover_landing/index.js
+++ javascript/landing_pages/hover_landing/index.js
@@ -3,13 +3,12 @@ const section = document.querySelector('.section'),
   sectionCol = section.querySelectorAll('.section_col'),
   sectionCaption = section.querySelectorAll('.section_col_caption')
 
-const isDesktop = window.matchMedia('(min-width: 768px)')
+const isDesktop = window.matchMedia('(min-width: 769px)')
 const init = () => {
   if (isDesktop.matches) addEventListeners()
 }
 
 const addEventListeners = () => {
-  console.log('calling listeners')
   // set active class to the first col
   sectionCol[0].classList.add('active')
 
@@ -24,3 +23,84 @@ const addEventListeners = () => {
   })
 }
 init()
+
+const menu = document.querySelector('.menu'),
+  menuItems = menu.querySelectorAll('.menu_list'),
+  menuItemsAnchor = menu.querySelectorAll('.menu_list_item')
+
+const openButton = document.querySelector('.menu_open')
+const closeButton = document.querySelector('.menu_close')
+
+const tl = gsap.timeline({
+  paused: true,
+  defaults: { duration: 0.92, ease: 'expo.inOut' },
+})
+
+const initMenu = () => {
+  gsap.set(menu, { pointerEvents: 'none', autoAlpha: 0 })
+  gsap.set(menuItems, { autoAlpha: 0 })
+  gsap.set(menuItemsAnchor, { x: '-100%' })
+
+  tl.to(
+    menu,
+    {
+      autoAlpha: 1,
+      stagger: 0.02,
+      pointerEvents: 'auto',
+    },
+    0
+  )
+    .to(
+      menuItems,
+      {
+        autoAlpha: 1,
+      },
+      0.08
+    )
+    .to(
+      menuItemsAnchor,
+      {
+        x: 0,
+        stagger: 0.016,
+      },
+      0
+    )
+
+  tl.to(
+    '.app',
+    {
+      x: '-50rem',
+    },
+    0
+  )
+    .to(
+      '.section_header div',
+      {
+        autoAlpha: 0,
+      },
+      0
+    )
+    .to(
+      'body',
+      {
+        backgroundColor: '#111111',
+        overflow: 'hidden',
+        pointerEvents: 'none',
+      },
+      0
+    )
+}
+
+const open = () => {
+  tl.play()
+  menu.computedStyleMap.pointerEvents = 'auto'
+}
+const close = () => {
+  tl.reverse()
+  menu.computedStyleMap.pointerEvents = 'none'
+}
+
+openButton.addEventListener('click', open)
+closeButton.addEventListener('click', close)
+
+initMenu()

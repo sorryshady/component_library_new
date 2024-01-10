ecfe59370b1263bd8160d24c04af180c96f6c8f2
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-01-10 15:58:30 +0530

    hover-landing nextjs menu animation part 2 remaining

--- next/landing_pages/hover_landing/app/components/Menu/Menu.js
+++ next/landing_pages/hover_landing/app/components/Menu/Menu.js
@@ -3,66 +3,90 @@ import gsap from 'gsap'
 import styles from './Menu.module.scss'
 import { menuData } from './menu_data'
 const Menu = ({ menuOpen, handleMenu, mainContainerRef }) => {
-  const menuRef = useRef(null)
-  console.log(menuOpen)
+  const menu = useRef()
+  const tl = useRef()
+
   useEffect(() => {
-    const menu = menuRef.current
-    const mainContainer = mainContainerRef.current
+    const menuContainer = gsap.utils.toArray('.menu_list_ref')
+    const anchors = gsap.utils.toArray('.menu_list_item_ref')
 
-    gsap.set(menu, { pointerEvents: 'none', autoAlpha: 0 })
+    gsap.set(menu.current, { pointerEvents: 'none', autoAlpha: 0 })
+    menuContainer.forEach((item) => {
+      gsap.set(item, { autoAlpha: 0 })
+    })
+    anchors.forEach((item) => {
+      gsap.set(item, { x: '-100%' })
+    })
 
-    const tl = gsap.timeline({
+    tl.current = gsap.timeline({
       paused: true,
       defaults: {
         duration: 0.92,
         ease: 'expo.inOut',
       },
-      onComplete: () => {
-        menu.computedStyleMap.pointerEvents = menuOpen ? 'auto' : 'none'
-        if (!menuOpen) {
-          handleMenu()
-        }
-      },
     })
 
-    tl.to(
-      menu,
-      {
-        autoAlpha: 1,
-        stagger: 0.02,
-        pointerEvents: 'auto',
-      },
-      0
-    )
+    tl.current
+      .to(
+        menu.current,
+        {
+          autoAlpha: 1,
+          stagger: 0.02,
+          pointerEvents: 'auto',
+        },
+        0
+      )
+      .to(
+        menuContainer,
+        {
+          autoAlpha: 1,
+        },
+        0.08
+      )
+      .to(
+        anchors,
+        {
+          x: 0,
+          stagger: 0.016,
+        },
+        0
+      )
 
-    tl.to(
-      mainContainer,
-      {
-        x: menuOpen ? '-50rem' : '0',
-      },
-      0
-    ).to(
-      'body',
-      {
-        backgroundColor: menuOpen ? '#111111' : 'var(--primary)',
-        overflow: menuOpen ? 'hidden' : 'auto',
-        pointerEvents: menuOpen ? 'none' : 'auto',
-      },
-      0
-    )
+    tl.current
+      .to(
+        mainContainerRef.current,
+        {
+          x: '-50rem',
+        },
+        0
+      )
+      .to(
+        'body',
+        {
+          backgroundColor: '#111111',
+          overflow: 'hidden',
+          pointerEvents: 'none',
+        },
+        0
+      )
+  }, [mainContainerRef])
 
-    if (menuOpen) {
-      tl.play()
-    } else {
-      tl.reverse()
-    }
-  }, [menuOpen, handleMenu, mainContainerRef])
+  useEffect(() => {
+    menuOpen ? tl.current.play() : tl.current.reverse()
+  }, [menuOpen])
 
   return (
-    <div className={styles.menu} ref={menuRef}>
+    <div className={styles.menu} ref={menu}>
       <div className={styles.menu_wrapper}>
         {menuData.map((data) => {
-          return <MenuList key={data.title} data={data} />
+          return (
+            <div
+              key={Math.random()}
+              className={`menu_list_ref ${styles.menu_list}`}
+            >
+              <MenuList data={data} />
+            </div>
+          )
         })}
         <div className='menu_close' onClick={() => handleMenu()}>
           <p>&#10005;</p>
@@ -76,14 +100,18 @@ export default Menu
 
 const MenuList = ({ data }) => {
   return (
-    <div className={styles.menu_list}>
+    <>
       {data.map((item) => {
         return (
-          <div key={item.id} className={styles.menu_list_item}>
+          <div
+            key={item.id}
+            className={`menu_list_item_ref ${styles.menu_list_item}`}
+          >
             <a href={item.link}>{item.title}</a>
           </div>
         )
       })}
-    </div>
+    </>
   )
 }
+
